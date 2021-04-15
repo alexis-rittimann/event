7aaf9777a53eb5d2b7c28b6b78e9971fe6642c8c
alexis <alexritti111@gmail.com>
2021-04-15 08:58:01 +0200

    first commit

--- front/js/app.js
+++ front/js/app.js
@@ -1,1 +1,29 @@
-console.log('hello');
+function afficherEvenement(evenement) {
+    document.getElementById("listeEvenements").innerHTML += `
+    <div class="event">
+        <h2>Titre : ${evenement.titre}</h2>
+        <p>${evenement.description}</p>
+    </div>`;
+}
+
+function creerEvenement(titre, debut, fin, desc, image) {
+    return {
+        titre: titre,
+        dateHeureDebut:  debut,
+        dateHeureFin: fin,
+        description : desc,
+        urlImage : image
+    }
+}
+
+let listeEvenements = [];
+
+listeEvenements.push(creerEvenement("Ev. 1", new Date(), new Date(), "Super événement 1", ""));
+listeEvenements.push(creerEvenement("Ev. 2", new Date(), new Date(), "Super événement 2", ""));
+
+
+for (let evenement of listeEvenements) {
+    afficherEvenement(evenement);
+}
+
+console.log(listeEvenements);
